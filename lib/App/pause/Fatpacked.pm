762d79693343875cadecc5e0ec3c6c6180314714
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-09 10:30:37 +0700

    Initial commit

---
+++ lib/App/pause/Fatpacked.pm
@@ -0,0 +1,24 @@
+package App::pause::FatPacked;
+
+# DATE
+# VERSION
+
+# PACKED_MODULES
+# PACKED_DISTS
+
+1;
+# ABSTRACT: A CLI for PAUSE (fatpacked version)
+
+=head1 DESCRIPTION
+
+This distribution is for testing/development purposes. Users should just install
+the L<App::pause> distribution.
+
+
+=head1 SEE ALSO
+
+L<App::pause>
+
+L<App::pause::>
+
+L<pause-fatpacked>
